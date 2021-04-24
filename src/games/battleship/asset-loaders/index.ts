55561c7e9b09b1c287e97bb3a6160fd9cb209fa5
ShelbyCat <dev.shelby.kauth@gmail.com>
2021-04-24 18:16:25 -0400

    Change starry backgroud

--- src/games/battleship/asset-loaders/index.ts
+++ src/games/battleship/asset-loaders/index.ts
@@ -1,4 +1,4 @@
-import starBackground from "~bts/assets/background_stars_03.png";
+import starBackground from "~bts/assets/background_stars_04.png";
 import board from "~bts/assets/board_01.png";
 import shipsAndMarkers from "~bts/assets/ships_and_markers_01.png";
 
